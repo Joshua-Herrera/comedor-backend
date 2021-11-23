b246a89ed29d8e6cd8f2dc2d0fdedcdb79a95202
Joshua-Herrera <91395285+Joshua-Herrera@users.noreply.github.com>
2021-11-23 09:26:03 -0600

    Add files via upload

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -12,84 +12,43 @@ router
     authController.restrictTo('admin', 'helper'),
     DashboardController.getStats
   );
-router
-  .route('/all')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin', 'helper'),
-    DashboardController.getAllStats
-  );
-  router
-  .route('/allByDay')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin', 'helper'),
-    DashboardController.getAllStatsDay
-  );
-router
-  .route('/range')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin', 'helper'),
-    DashboardController.getRangedStats
-  );
-router
-  .route('/day')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin', 'helper'),
-    DashboardController.getDailyStats
-  );
-router
-  .route('/week')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin', 'helper'),
-    DashboardController.getAllWeeklyStats
-  );
-router
-  .route('/months')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin', 'helper'),
-    DashboardController.getAllMonthStats
-  );
-router
-  .route('/years')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin', 'helper'),
-    DashboardController.getAllYearsStats
-  );
 
-  router
+router
+  .route('/linechart')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'helper'),
+    DashboardController.selledDishesPerDay
+  )
+
+router
   .route('/outcome/day')
   .get(
     authController.protect,
     authController.restrictTo('admin', 'helper'),
     StatsController.getStatsDay
   );
-  router
+router
   .route('/outcome/week')
   .get(
     authController.protect,
     authController.restrictTo('admin', 'helper'),
     StatsController.getStatsWeek
   );
-  router
+router
   .route('/outcome/month')
   .get(
     authController.protect,
     authController.restrictTo('admin', 'helper'),
     StatsController.getStatsMonth
   );
-  router
+router
   .route('/outcome/year')
   .get(
     authController.protect,
     authController.restrictTo('admin', 'helper'),
     StatsController.getStatsYear
   );
-  
+
 
 module.exports = router;
